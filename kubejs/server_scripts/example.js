f6d8535aa77dcd58ffd56d3f50749f49789370cd
So1Sauc3 <111549280+So1Sauc3@users.noreply.github.com>
2023-12-16 16:32:11 -0600

    Add files via upload

---
+++ kubejs/server_scripts/example.js
@@ -0,0 +1,35 @@
+ServerEvents.recipes(event => {
+    event.remove({ output: 'createdeco:brass_coin' })
+    event.remove({ output: 'createdeco:copper_coin' })
+    event.remove({ output: 'createdeco:netherite_coin' })
+    event.remove({ output: 'ars_nouveau:warp_scroll' })
+    event.remove({ output: 'tempad:tempad' })
+    
+    event.shapeless('kubejs:example_iron_farm', [
+    'create:empty_schematic',
+    'minecraft:iron_ingot'
+    ])
+
+    event.shapeless('kubejs:example_gold_farm', [
+        'create:empty_schematic',
+        'minecraft:gold_ingot'
+    ])
+    
+    event.recipes.create.compacting('kubejs:oreo_thin', ['kubejs:oreo_wafer_top','kubejs:oreo_cream','kubejs:oreo_wafer_bottom']).superheated()
+
+    event.recipes.createMechanicalCrafting('minecraft:enchanted_golden_apple', [
+		' AAA ',
+		'AABAA',
+		'ABCBA',
+        'AABAA',
+        ' AAA '
+	], {
+		A: 'minecraft:gold_block',
+		B: 'create:experience_block',
+		C: 'create:honeyed_apple'
+	})
+})
+LootJS.modifiers(event => {
+    event.addLootTableModifier(/.*/)
+        .removeLoot('tempad:he_who_remains_tempad')
+})
---
+++ kubejs/startup_scripts/example.js
@@ -0,0 +1,10 @@
+StartupEvents.registry('item', e => {
+    e.create('example_iron_farm')
+    e.create('example_gold_farm')
+    e.create('oreo_thin').rarity('epic').unstackable()
+    e.create('oreo_wafer_bottom')
+    e.create('oreo_wafer_top')
+    e.create('oreo_cream')
+    
+})
+
